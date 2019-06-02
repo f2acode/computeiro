2b19bdabca190d287dbbb573215f950adbfcf32e
felipeaugustox <augustto.felipe@hotmail.com>
2019-06-02 02:18:05 -0300

    feat: add finish exam

--- lib/components/pages/index.dart
+++ lib/components/pages/index.dart
@@ -3,3 +3,4 @@ export 'package:computeiro/components/pages/DownloadsView/index.dart';
 export 'package:computeiro/components/pages/About/index.dart';
 export 'package:computeiro/components/pages/ExamsList/index.dart';
 export 'package:computeiro/components/pages/Exam/index.dart';
+export 'package:computeiro/components/pages/FinishExamOverview/index.dart';
