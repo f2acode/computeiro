c2194b0792dbca3902bad2a351d6ed32aaf94424
Felipe Augusto <augustto.felipe@hotmail.com>
2020-11-05 13:01:08 -0300

    change icon and colors

--- lib/core/style/computeiro_colors.dart
+++ lib/core/style/computeiro_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color darkGray = Color.fromRGBO(45, 64, 89, 1);
-const Color yellow = Color.fromRGBO(255, 180, 0, 1);
+const Color darkGray = Color.fromRGBO(1, 169, 125, 1);
+const Color yellow = Color.fromRGBO(255, 200, 68, 1);
 const Color lightGray = Color.fromRGBO(246, 246, 246, 1);
 const Color pink = Color.fromRGBO(234, 84, 85, 1);
